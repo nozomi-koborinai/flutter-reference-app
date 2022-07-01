50c127d9eb594763ab66367b511494681bdfb746
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-07-01 23:45:00 +0900

    すささんのコードレビュー結果反映：データ層のモデルとドメイン層のモデルを分けた

--- lib/data_layer/models/post_document.g.dart
+++ lib/data_layer/models/post_document.g.dart
@@ -1,17 +1,19 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'post.dart';
+part of 'post_document.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$_Post _$$_PostFromJson(Map<String, dynamic> json) => _$_Post(
+_$_PostDocument _$$_PostDocumentFromJson(Map<String, dynamic> json) =>
+    _$_PostDocument(
       json['content'] as String,
-      json['accountId'] as String,
+      json['account_id'] as String,
     );
 
-Map<String, dynamic> _$$_PostToJson(_$_Post instance) => <String, dynamic>{
+Map<String, dynamic> _$$_PostDocumentToJson(_$_PostDocument instance) =>
+    <String, dynamic>{
       'content': instance.content,
-      'accountId': instance.accountId,
+      'account_id': instance.accountId,
     };
--- lib/domain_layer/models/post.dart
+++ lib/domain_layer/models/post.dart
@@ -7,8 +7,8 @@ part 'post.g.dart';
 @freezed
 class Post with _$Post {
   const factory Post(
-    @JsonKey(name: 'content') String content,
-    @JsonKey(name: 'account_id') String accountId,
+    String content,
+    String accountId,
   ) = _Post;
 
   factory Post.fromJson(Map<String, dynamic> json) => _$PostFromJson(json);
