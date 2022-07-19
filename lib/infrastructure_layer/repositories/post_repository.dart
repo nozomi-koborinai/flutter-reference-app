4f5e711269f9160969ac39f9b368e6f84bd14c18
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-07-20 01:53:49 +0900

    不要import文の削除

--- lib/infrastructure_layer/repositories/post_repository.dart
+++ lib/infrastructure_layer/repositories/post_repository.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:repository_riverpod_mvvm/domain_layer/interfaces/i_post_repository.dart';
 import 'package:repository_riverpod_mvvm/domain_layer/models/post.dart';
-import 'package:repository_riverpod_mvvm/utils/app_values.dart';
 
 class PostRepository implements IPostRepository {
   PostRepository({
