dc6e46818e761a32c68311ed651ee98a020bee50
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-12-25 16:09:31 +0900

    PostRepositoryのUnitTestを追加

---
+++ test/infrastructure/post_repostitory_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:repository_riverpod_mvvm/domain_layer/domain_providers.dart';
+import 'package:repository_riverpod_mvvm/domain_layer/models/post.dart';
+import 'mock_post_repository.dart';
+
+void main() {
+  final container = ProviderContainer(
+  overrides: [
+    postRepositoryProvider.overrideWithValue(MockPostRepository()),
+  ],
+);
+    group('postRepositoryProvider', () {
+    final mockRepo = container.read(postRepositoryProvider);
+
+    test('投稿データが追加されるはず', () async {
+      await mockRepo.addPost(const Post(id: '0001', content: 'test1', accountId: 'account1'));
+      await mockRepo.addPost(const Post(id: '0002', content: 'test2', accountId: 'account2'));
+      final posts = (mockRepo as MockPostRepository).posts;
+      expect(posts.length, 2);
+      expect(posts.first.id, '0001');
+      expect(posts.last.id, '0002');
+    });
+    test('0001のPostデータが取得されるはず', () async {
+      final fetchPost =  await  mockRepo.fetchPostFromUserId('0001');
+        expect(fetchPost.length, 1);
+        expect(fetchPost.first.content, 'test1');
+      });
+    });
+  }
