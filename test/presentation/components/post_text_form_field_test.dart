e4e12d78ac32839402ba891c100f81d12708a5fc
nozomi-koborinai <kobofender@gmail.com>
2023-03-07 19:12:31 +0900

    💚 test: ディレクトリ構成変更に伴うimport文変更

--- test/presentation/components/post_text_form_field_test.dart
+++ test/presentation/components/post_text_form_field_test.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:riverpod_layered_architecture/application/state/selected_post.dart';
 import 'package:riverpod_layered_architecture/domain/models/post.dart';
 import 'package:riverpod_layered_architecture/presentation/pages/post/components/post_text_form_field.dart';
-import 'package:riverpod_layered_architecture/presentation/pages/post/post_page.dart';
 
 import '../../test_agent.dart';
 
@@ -36,8 +36,9 @@ void main() {
   // TODO:テストが通らないので直す
   group('投稿編集時のWidgetテスト', () {
     testWidgets('テキストに選択中の投稿内容が設定されること', (WidgetTester tester) async {
-      ProviderContainer().read(selectedPostProvider.notifier).state =
-          const Post(content: '投稿内容', contributor: '投稿者');
+      ProviderContainer()
+          .read(selectedPostProvider.notifier)
+          .set(const Post(content: '投稿内容', contributor: '投稿者'));
       await tester.pumpWidget(
         ProviderScope(
           overrides: agent.providerOverrides,
--- test/test_agent.dart
+++ test/test_agent.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_layered_architecture/application/state/selected_post.dart';
 import 'package:riverpod_layered_architecture/domain/repositories/post_repository.dart';
-import 'package:riverpod_layered_architecture/presentation/pages/post/post_page.dart';
 
 import 'infrastructure/mocks/mock_post_repository.dart';
 
