1c658f73a2f8dc55277f2c038f3189830b239c8b
nozomi-koborinai <kobofender@gmail.com>
2023-04-20 11:29:40 +0900

    ♻️ refactor: プレゼンテーション層の画面表示系の処理実装
    #8

--- lib/presentation/pages/post/components/post_button.dart
+++ lib/presentation/pages/post/components/post_button.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_reference_app/presentation/pages/post/components/post_text_form_field.dart';
 import 'package:flutter_reference_app/presentation/view_utils.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../application/post_service.dart';
 import '../../../../application/state/selected_post.dart';
+import '../../../router_config.dart';
 
 class PostButton extends ConsumerWidget {
   const PostButton({super.key});
@@ -34,12 +36,23 @@ class PostButton extends ConsumerWidget {
             );
           }
         } else {
-          await ref.read(postServiceProvider).updatePost(
-                id: selectedPost.id!,
-                content: contentController.text,
-                contributor: contributorController.text,
-              );
+          try {
+            await ref.read(postServiceProvider).updatePost(
+                  id: selectedPost.id!,
+                  content: contentController.text,
+                  contributor: contributorController.text,
+                );
+            viewUtils.showSnackBar(
+              message: '投稿内容を編集しました',
+            );
+          } catch (e) {
+            viewUtils.showSnackBar(
+              message: e.toString(),
+              mode: SnackBarMode.failure,
+            );
+          }
         }
+        context.goNamed(RouteConfigs.timeLine.name);
       },
       child: Text(selectedPost == null ? '投稿' : '保存'),
     );
