caa50f7de97f50c60be4a5c704b8ef0804cd027a
nozomi-koborinai <kobofender@gmail.com>
2023-04-20 10:49:24 +0900

    🐛 fix: ProviderRef -> Ref
    #8

--- lib/application/run_usecase_mixin.dart
+++ lib/application/run_usecase_mixin.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// ユースケース実行のためのメソッドを備えた Mixin
 mixin RunUsecaseMixin {
-  Future<T> execute<T>(ProviderRef ref, Future<T> Function() action) async {
+  Future<T> execute<T>(Ref ref, Future<T> Function() action) async {
     ref.read(overlayLoadingProvider.notifier).update((_) => true);
     try {
       return await action();
